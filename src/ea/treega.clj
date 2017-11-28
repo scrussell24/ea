52804411499437bb7eca08fa46b09222f6213c19
Scott Russell <scott@Scotts-MacBook.local>
2017-11-27 23:16:53 -0600

    fix index

--- src/ea/treega.clj
+++ src/ea/treega.clj
@@ -15,7 +15,7 @@
 (defn mate 
   [chrm1 chrm2 chrm-fns]
   (let [index1 (rand-int-between 1 ((chrm-fns :count-chrm) chrm1))
-        index2 (rand-int-between 1 ((chrm-fns :count-chrm) chrm1))
+        index2 (rand-int-between 1 ((chrm-fns :count-chrm) chrm2))
         node (last ((chrm-fns :split-chrm) index2 chrm2))]
     ((chrm-fns :mutate) (vec ((chrm-fns :concat-chrm) chrm1 index1 node)) chrm-fns)))
 
